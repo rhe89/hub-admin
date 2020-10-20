e4f0fa1fbad0f0fd64655f1e8568323622f54b6e
Roar <roar.h.eriksen@gmail.com>
2020-10-20 22:50:55 +0200

    Update NuGet-package

--- Admin.Web.Ui/Program.cs
+++ Admin.Web.Ui/Program.cs
@@ -3,11 +3,11 @@ using Microsoft.Extensions.Hosting;
 
 namespace Admin.Web.Ui
 {
-    public class Program : WebHostBuilder<Startup, DependencyRegistrationFactory>
+    public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            new WebHostBuilder<Startup, DependencyRegistrationFactory>().CreateHostBuilder(args).Build().Run();
         }
     }
 }
