5d61703ff5793af4c3eefb846564e76bd31ee28b
Roar <roar.h.eriksen@gmail.com>
2020-10-20 23:41:11 +0200

    Update Program.cs

--- Admin.Web.Ui/Program.cs
+++ Admin.Web.Ui/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Admin.Web.Ui
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
